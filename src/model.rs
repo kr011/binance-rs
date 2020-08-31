371c57226b37f28204305c6ec943dcd3f1e92a6d
K.R <kr@0pen.org>
2020-08-30 17:22:34 -0700

    kr

--- src/model.rs
+++ src/model.rs
@@ -924,3 +924,59 @@ pub struct FuturesCancelAllResp {
     pub code: i32,
     pub msg: String,
 }
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct AccountInformationV2 {
+    pub fee_tier: usize,
+    pub can_trade: bool,
+    pub can_deposit: bool,
+    pub can_withdraw: bool,
+    pub update_time: u64,
+    pub total_initial_margin: String,
+    pub total_maint_margin: String,
+    pub total_wallet_balance: String,
+    pub total_unrealized_profit: String,
+    pub total_margin_balance: String,
+    pub total_position_initial_margin: String,
+    pub total_open_order_initial_margin: String,
+    pub total_cross_wallet_balance: String,
+    pub total_cross_un_pnl: String,
+    pub available_balance: String,
+    pub max_withdraw_amount: String,
+    pub assets: Vec<AssetV2>,
+    pub positions: Vec<PositionV2>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct AssetV2 {
+    pub asset: String,
+    pub wallet_balance: String,
+    pub unrealized_profit: String
+    pub margin_balance: String,
+    pub maint_margin: String,
+    pub initial_margin: String,
+    pub position_initial_margin: String,
+    pub openOrder_initial_margin: String,
+    pub cross_wallet_balance: String,
+    pub cross_un_pnl: String,
+    pub available_balance: String,
+    pub max_withdraw_amount: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct PositionV2 {
+    pub symbol: String,
+    pub initial_margin: String,
+    pub maint_margin: String,
+    pub unrealized_profit: String,
+    pub position_initial_margin: String,
+    pub open_order_initial_margin: String,
+    pub leverage: String,
+    pub isolated: bool,
+    pub entry_price: String,
+    pub max_notional: String,
+    pub position_side: String,
+}
